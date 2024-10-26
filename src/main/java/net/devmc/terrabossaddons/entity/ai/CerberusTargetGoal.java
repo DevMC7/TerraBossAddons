d300e921059f53a8eba09c746d146dbd09049814
DevMC7 <157959675+DevMC7@users.noreply.github.com>
2024-10-26 11:57:16 +0300

    Targeting AI update

--- src/main/java/net/devmc/terrabossaddons/entity/ai/CerberusTargetGoal.java
+++ src/main/java/net/devmc/terrabossaddons/entity/ai/CerberusTargetGoal.java
@@ -26,30 +26,30 @@ public class CerberusTargetGoal extends ActiveTargetGoal<LivingEntity> {
 
 	@Override
 	protected void findClosestTarget() {
-		Box box = new Box(new BlockPos((int) this.mob.getX(), (int) this.mob.getEyeY(), (int) this.mob.getZ()));
-		box.expand(this.getFollowRange(), 4F, this.getFollowRange());
-		List<LivingEntity> entities = this.cerberus.getWorld().getEntitiesByClass(LivingEntity.class, box, livingEntity -> livingEntity.isAlive() && !(livingEntity instanceof CerberusBoss));
-		LivingEntity target =
-				this.cerberus.getWorld().getClosestEntity(this.mob.getWorld()
-				.getEntitiesByClass(
-						this.targetClass,
-						this.getSearchBox(this.getFollowRange()),
-						(livingEntity) -> true),
-				this.targetPredicate,
-				this.mob,
-				this.mob.getX(),
-				this.mob.getEyeY(),
-				this.mob.getZ());
+		Box box = new Box(new BlockPos((int) this.mob.getX(), (int) this.mob.getEyeY(), (int) this.mob.getZ()))
+				.expand(this.getFollowRange(), 4.0F, this.getFollowRange());
+
+		List<LivingEntity> entities = this.cerberus.getWorld().getEntitiesByClass(
+				LivingEntity.class,
+				box,
+				entity -> entity.isAlive() && !(entity instanceof CerberusBoss)
+		);
+		LivingEntity target = null;
+
+		if (cerberus.getRecentDamageSource() != null &&
+				cerberus.getRecentDamageSource().getAttacker() instanceof LivingEntity lastAttacker) {
+			target = lastAttacker; // set target to the latest attacker
+		}
+
 		for (LivingEntity entity : entities) {
-			float distance = 3 * Math.max(2, Math.max(1, cerberus.getAnger()) / 20);
-			if (entity.hasStatusEffect(StatusEffects.INVISIBILITY)) distance *= 0.5f;
-			if (entity instanceof PlayerEntity player) distance *= Math.max(1, cerberus.getAngerLeveLMultiplier(player) / 1.5f);
-			if (cerberus.getRecentDamageSource() != null &&
-					cerberus.getRecentDamageSource().getAttacker() != null &&
-					cerberus.getRecentDamageSource().getAttacker() instanceof LivingEntity lastAttacker) target = lastAttacker;
-			if (cerberus.distanceTo(entity) <= distance) {
+			float adjustedDistance = 3 * Math.max(2, Math.max(1, cerberus.getAnger()) / 20);
+			
+			if (entity.hasStatusEffect(StatusEffects.INVISIBILITY)) adjustedDistance *= 0.5f;
+			if (entity instanceof PlayerEntity player) adjustedDistance *= Math.max(1, cerberus.getAngerLeveLMultiplier(player) / 1.5f);
+
+			if (cerberus.distanceTo(entity) <= adjustedDistance) {
 				target = entity;
-                break;
+				break;
 			}
 		}
 

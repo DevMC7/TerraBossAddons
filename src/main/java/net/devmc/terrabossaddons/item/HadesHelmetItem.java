bda78e645451dd5e9ef768d5a38a40793c7f47ce
DevMC7 <devmc.ytc@gmail.com>
2025-01-13 15:21:08 +0200

    Hades helmet update
    - Set item rarity to rare
    - Properly set the item's protection

--- src/main/java/net/devmc/terrabossaddons/item/HadesHelmetItem.java
+++ src/main/java/net/devmc/terrabossaddons/item/HadesHelmetItem.java
@@ -1,16 +1,28 @@
 package net.devmc.terrabossaddons.item;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.UUID;
 
 public class HadesHelmetItem extends ArmorItem {
 
 	public HadesHelmetItem() {
-		super(ArmorMaterials.NETHERITE, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(Integer.MAX_VALUE));
+		super(ArmorMaterials.NETHERITE, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(Integer.MAX_VALUE).rarity(Rarity.RARE));
+		this.attributeModifiers.put(
+				EntityAttributes.GENERIC_ARMOR,
+				new EntityAttributeModifier(
+                        UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150"), // helmet armor modifier
+						"Armor modifier",
+						getProtection(),
+						EntityAttributeModifier.Operation.ADDITION));
 	}
 
 	@Override
@@ -20,7 +32,7 @@ public class HadesHelmetItem extends ArmorItem {
 
 	@Override
 	public boolean canRepair(ItemStack stack, ItemStack ingredient) {
-		return false;
+		return stack.isOf(this) && ingredient.isOf(this);
 	}
 
 	@Override
